6aa319fc89638eb8837f7276020e5db6551f8600
hyogij <hyogij@gmail.com>
2015-07-13 11:42:25 +0900

    Adds a temporary solution for counting and saying

---
+++ leetcode.com/Day7.CountAndSay/src/leetCode/CountAndSay.java
@@ -0,0 +1,32 @@
+package leetCode;
+
+/*
+ ==========================================================
+ Author : Hyogi Jung(hyogij@gmail.com)
+ Date : 2015.07.13
+ Question Number : 28
+ Link : https://leetcode.com/problems/count-and-say/
+ ==========================================================
+ */
+public class CountAndSay {
+	/*
+	 * The count-and-say sequence is the sequence of integers beginning as
+	 * follows: 1, 11, 21, 1211, 111221, ...
+	 * 
+	 * 1 is read off as "one 1" or 11. 11 is read off as "two 1s" or 21. 21 is
+	 * read off as "one 2, then one 1" or 1211. Given an integer n, generate the
+	 * nth sequence.
+	 * 
+	 * Note: The sequence of integers will be represented as a string.
+	 */
+	public static void main(String[] args) {
+		int n = 111221;
+		System.out.println("countAndSay " + countAndSay(n));
+	}
+
+	public static String countAndSay(int n) {
+		StringBuffer say = new StringBuffer();
+
+		return say.toString();
+	}
+}
