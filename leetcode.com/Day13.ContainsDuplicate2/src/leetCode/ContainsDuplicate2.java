bd47cc96b8e7831c62af872a0bce21e93b61089c
hyogij <hyogij@gmail.com>
2015-08-05 14:20:11 +0900

    Adds a template to solve leetCode problem

---
+++ leetcode.com/Day13.ContainsDuplicate2/src/leetCode/ContainsDuplicate2.java
@@ -0,0 +1,26 @@
+package leetCode;
+
+/*
+ ==========================================================
+ Author : Hyogi Jung(hyogij@gmail.com)
+ Date : 2015.08.05
+ Question Number : 58
+ Link : https://leetcode.com/problems/contains-duplicate-ii/
+ ==========================================================
+ */
+
+public class ContainsDuplicate2 {
+	/*
+	 * Given an array of integers and an integer k, find out whether there there
+	 * are two distinct indices i and j in the array such that nums[i] = nums[j]
+	 * and the difference between i and j is at most k.
+	 */
+
+	public static void main(String[] args) {
+		System.out.println(containsNearbyDuplicate(, ));
+	}
+
+	public static boolean containsNearbyDuplicate(int[] nums, int k) {
+		return true;
+	}
+}
