d2ce50b50c9e7de171366d19eac5a6f628d0a83d
hyogij <hyogij@gmail.com>
2015-09-10 16:28:11 +0900

    Find the maximal product of any triplet

---
+++ codility/Lesson3.GenomicRangeQuery/src/codility/GenomicRangeQuery.java
@@ -0,0 +1,21 @@
+package codility;
+
+/*
+==========================================================
+Author : Hyogi Jung(hyogij@gmail.com)
+Date : 2015.09.10
+Lesson Number : 11
+Link : https://codility.com/demo/take-sample-test/genomic_range_query/
+==========================================================
+*/
+
+public class GenomicRangeQuery {
+
+	public static void main(String[] args) {
+		System.out.println("solution " + solution(0, 1, 11));
+	}
+
+	public int solution(int[] A) {
+		
+	}
+}
