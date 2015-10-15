ebe38ff3623a82dca72feb3d5792896e3e089b54
hyogij <hyogij@gmail.com>
2015-10-15 16:51:07 +0900

    Add the temporary solution to find all possible unique permutations

---
+++ leetcode.com/Day39.Permute2/src/leetCode/Permute2.java
@@ -0,0 +1,63 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ ==========================================================
+ Author : Hyogi Jung(hyogij@gmail.com)
+ Date : 2015.10.15
+ Question Number : 146
+ Link : https://leetcode.com/problems/permutations-ii/
+ ==========================================================
+ */
+
+public class Permute2 {
+	/*
+	 * Given a collection of numbers that might contain duplicates, return all
+	 * possible unique permutations.
+	 * 
+	 * For example, [1,1,2] have the following unique permutations: [1,1,2],
+	 * [1,2,1], and [2,1,1].
+	 */
+	public static void main(String[] args) {
+		int[] candidates = {1, 1, 2};
+		List<List<Integer>> permutes = permuteUnique(candidates);
+		System.out.println(permutes.toString());
+	}
+
+	public static List<List<Integer>> permuteUnique(int[] candidates) {
+		if (candidates.length == 0) {
+			return null;
+		}
+
+		List<List<Integer>> permuteLists = new ArrayList<List<Integer>>();
+		List<Integer> permutes = new ArrayList<Integer>();
+		permuteHelper(permuteLists, permutes, candidates);
+		return permuteLists;
+	}
+
+	public static void permuteHelper(List<List<Integer>> permuteLists,
+			List<Integer> permutes, int[] candidates) {
+		// Add permute result to permute list if permute's size is the number of
+		// candidates and finish recursive method
+		if (permutes.size() == candidates.length) {
+			System.out.println(permutes.toString());
+			if (permuteLists.contains(permutes) != false) {
+				permuteLists.add(permutes);
+			}
+			return;
+		}
+
+		for (int i = 0; i < candidates.length; i++) {
+			int candidate = candidates[i];
+			// Add the candidate to permute result if it is not contained
+			// permute result
+			if (!permutes.contains(candidate)) {
+				List<Integer> newPermutes = new ArrayList<Integer>(permutes);
+				newPermutes.add(candidate);
+				permuteHelper(permuteLists, newPermutes, candidates);
+			}
+		}
+	}
+}
