7a5095fd22364beb7e14c39378c5d49f42188eb4
hyogij <hyogij@gmail.com>
2016-01-20 16:18:44 +0900

    Adds an another algorithm that uses recursive method

--- leetcode.com/Day32.NumDecodings/src/leetCode/NumDecodings.java
+++ leetcode.com/Day32.NumDecodings/src/leetCode/NumDecodings.java
@@ -22,21 +22,61 @@ public class NumDecodings {
 	 * The number of ways decoding "12" is 2.
 	 */
 	public static void main(String[] args) {
-		String s = "12111212";
-		System.out.println("numDecodings " + numDecodings(s));
+		String s1 = "1212121211";
+		System.out.println("numDecodingsDynamic " + numDecodingsDynamic(s1));
+		System.out.println("numDecodings " + numDecodings(s1));
+
+		String s2 = "11111";
+		System.out.println("numDecodingsDynamic " + numDecodingsDynamic(s2));
+		System.out.println("numDecodings " + numDecodings(s2));
 	}
 
+	// How can I solve this problem?
+	// 2612 --> ZAB, ZL
+	// 11111 --> AAAAA, AKAA, AKK, KAAA, KKA
+	// 121 --> A, L, AB, AU, LA, ABA
+	// Recursive method
+	// If string could be decoded, then return 1
+	// All input string could be decoded? There are edge examples : 010, 100
+	// Can I ignore these cases?
+	// But this algorithm exceed time limitations
 	public static int numDecodings(String s) {
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+
+		return numDecodingsHelper(s);
+	}
+
+	public static int numDecodingsHelper(String s) {
+		if (s.length() == 0) {
+			return 1;
+		}
+
+		int digitTwo = 0;
+		if (s.length() >= 2) {
+			digitTwo = Integer.valueOf(s.substring(0, 2));
+		}
+
+		if (s.length() >= 2 && digitTwo <= 26) {
+			return numDecodingsHelper(s.substring(1, s.length()))
+					+ numDecodingsHelper(s.substring(2, s.length()));
+		} else {
+			return numDecodingsHelper(s.substring(1, s.length()));
+		}
+	}
+
+	public static int numDecodingsDynamic(String s) {
 		if (s.length() == 0)
 			return 0;
 		int n = s.length();
 		int[] A = new int[n + 1];
 		A[n] = 1;
-		
-		if(s.charAt(n - 1) != '0') {
+
+		if (s.charAt(n - 1) != '0') {
 			A[n - 1] = 1;
 		}
-		
+
 		for (int i = n - 2; i >= 0; i--) {
 			if (s.charAt(i) != '0') {
 				A[i] = A[i + 1];
