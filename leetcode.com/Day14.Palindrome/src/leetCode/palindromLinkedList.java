c27a9d08daf8fb7e10ceddd2c13ec743f3d0b16e
hyogij <hyogij@gmail.com>
2016-01-21 15:32:16 +0900

    Adds another algorithm using stack

--- leetcode.com/Day14.Palindrome/src/leetCode/palindromLinkedList.java
+++ leetcode.com/Day14.Palindrome/src/leetCode/palindromLinkedList.java
@@ -1,5 +1,7 @@
 package leetCode;
 
+import java.util.Stack;
+
 /*
  ==========================================================
  Author : Hyogi Jung(hyogij@gmail.com)
@@ -16,18 +18,78 @@ public class palindromLinkedList {
 	 * Follow up: Could you do it in O(n) time and O(1) space?
 	 */
 	public static void main(String[] args) {
+		testEvenListNode();
+		testOddListNode();
+	}
+
+	public static void testEvenListNode() {
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(2);
+		ListNode l3 = new ListNode(31);
+		ListNode l4 = new ListNode(31);
+		ListNode l5 = new ListNode(2);
+		ListNode l6 = new ListNode(1);
+
+		l1.next = l2;
+		l2.next = l3;
+		l3.next = l4;
+		l4.next = l5;
+		l5.next = l6;
+
+		System.out.println("isPalindrome " + isPalindromeStack(l1));
+		System.out.println("isPalindrome " + isPalindrome(l1));
+	}
+
+	public static void testOddListNode() {
 		ListNode l1 = new ListNode(1);
 		ListNode l2 = new ListNode(2);
 		ListNode l3 = new ListNode(3);
 		ListNode l4 = new ListNode(2);
-		ListNode l5 = new ListNode(2);
+		ListNode l5 = new ListNode(1);
 
 		l1.next = l2;
 		l2.next = l3;
 		l3.next = l4;
 		l4.next = l5;
 
+		System.out.println("isPalindrome " + isPalindromeStack(l1));
 		System.out.println("isPalindrome " + isPalindrome(l1));
+	}
+
+	// Make new number and check the palindrome in nubmer. In this case, there
+	// are some problems when list has negative numbers.
+	// If we use stringbuffer class, it also has potential errors. If list node
+	// value is "100", its reverse string is "001". It returns false.
+	public static boolean isPalindromeStack(ListNode head) {
+		// We use stack data structure, but it requires space.
+		Stack<Integer> stack = new Stack<Integer>();
+		ListNode slow = head, fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+
+		// Slow points the middle of linked list
+		ListNode node = head;
+		while (node != slow) {
+			stack.push(node.val);
+			node = node.next;
+		}
+
+		// odd/even linked list
+		if (fast != null && fast.next == null) {
+			slow = slow.next;
+		}
+
+		while (stack.isEmpty() == false) {
+			int val = stack.pop();
+			if (val != slow.val) {
+				return false;
+			}
+			slow = slow.next;
+		}
+
+		return true;
 	}
 
 	public static boolean isPalindrome(ListNode head) {
