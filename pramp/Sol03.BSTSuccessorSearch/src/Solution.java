4e81e6909fe8258383187ba5f172b0d7a329df48
hyogij <hyogij@gmail.com>
2016-02-11 11:40:26 +0900

    Find inorder successor in BST

--- pramp/Sol03.BSTSuccessorSearch/src/Solution.java
+++ pramp/Sol03.BSTSuccessorSearch/src/Solution.java
@@ -23,13 +23,67 @@ public class Solution {
 		head.right.left = new TreeNode(12);
 		head.right.right = new TreeNode(17);
 
-		TreeNode n = new TreeNode(5);
+		TreeNode n = head.right;
 		TreeNode node = findParentNode(head, n);
 		System.out.println("findParentNode : " + node.val);
+		node = getInorderSuccessor(head, n);
+		System.out.println("getInorderSuccessor : " + node.val);
 		System.out.println("inoderTraversalRecusive");
 		inoderTraversalRecusive(head);
 		System.out.println("\ninoderTraversalIterative");
 		inoderTraversalIterative(head);
+	}
+
+	// Parent node pointer doesn't exist, input arguments are TreeNode
+	// i) Hasn't right child
+	// i-i) Node is a left child of parent node : parent node
+	// i-ii) Node is a right child of parent node : traversal the parent
+	// nodes until meet the right child
+	// ii) Has right child : left most child of node's right child
+	private static TreeNode getInorderSuccessor(TreeNode head, TreeNode node) {
+		if (node.right != null) {
+			return getLeftMostNode(node.right);
+		}
+
+		TreeNode successor = null;
+		TreeNode root = head;
+		while (node != null) {
+			if (root.val > node.val) {
+				successor = root;
+				root = root.left;
+			} else if (root.val < node.val) {
+				root = root.right;
+			} else {
+				break;
+			}
+		}
+		return successor;
+	}
+
+	private static TreeNode getLeftMostNode(TreeNode head) {
+		while (head.left != null) {
+			head = head.left;
+		}
+		return head;
+
+	}
+
+	private static TreeNode findParentNode(TreeNode head, TreeNode n) {
+		TreeNode successor = null;
+		TreeNode node = head;
+		while (node != null) {
+			if (node.val == n.val) {
+				return successor;
+			}
+
+			successor = node;
+			if (node.val < n.val) {
+				node = node.right;
+			} else if (node.val > n.val) {
+				node = node.left;
+			}
+		}
+		return null;
 	}
 
 	// Recursive method
@@ -62,24 +116,6 @@ public class Solution {
 		}
 
 		return;
-	}
-
-	private static TreeNode findParentNode(TreeNode head, TreeNode n) {
-		TreeNode successor = null;
-		TreeNode node = head;
-		while (node != null) {
-			if (node.val == n.val) {
-				return successor;
-			}
-
-			successor = node;
-			if (node.val < n.val) {
-				node = node.right;
-			} else if (node.val > n.val) {
-				node = node.left;
-			}
-		}
-		return null;
 	}
 
 	public static class TreeNode {
