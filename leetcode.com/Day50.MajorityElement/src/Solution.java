d361ca73d46ba3cf8fc4924b7ed2ef1e4c04af7e
hyogij <hyogij@gmail.com>
2016-02-15 17:44:16 +0900

    Modifies some codes

--- leetcode.com/Day50.MajorityElement/src/Solution.java
+++ leetcode.com/Day50.MajorityElement/src/Solution.java
@@ -15,14 +15,14 @@ public class Solution {
 	 * always exist in the array.
 	 */
 	public static void main(String[] args) {
-		int[] num = {47, 47, 72, 47, 72, 47, 79, 47, 12, 92, 13, 47, 47, 83,
-				33, 15, 18, 47, 47, 47, 47, 64, 47, 65, 47, 47, 47, 47, 70, 47,
-				47, 55, 47, 15, 60, 47, 47, 47, 47, 47, 46, 30, 58, 59, 47, 47,
-				47, 47, 47, 90, 64, 37, 20, 47, 100, 84, 47, 47, 47, 47, 47,
-				89, 47, 36, 47, 60, 47, 18, 47, 34, 47, 47, 47, 47, 47, 22, 47,
-				54, 30, 11, 47, 47, 86, 47, 55, 40, 49, 34, 19, 67, 16, 47, 36,
-				47, 41, 19, 80, 47, 47, 27};
-
+		// int[] num = {47, 47, 72, 47, 72, 47, 79, 47, 12, 92, 13, 47, 47, 83,
+		// 33, 15, 18, 47, 47, 47, 47, 64, 47, 65, 47, 47, 47, 47, 70, 47,
+		// 47, 55, 47, 15, 60, 47, 47, 47, 47, 47, 46, 30, 58, 59, 47, 47,
+		// 47, 47, 47, 90, 64, 37, 20, 47, 100, 84, 47, 47, 47, 47, 47,
+		// 89, 47, 36, 47, 60, 47, 18, 47, 34, 47, 47, 47, 47, 47, 22, 47,
+		// 54, 30, 11, 47, 47, 86, 47, 55, 40, 49, 34, 19, 67, 16, 47, 36,
+		// 47, 41, 19, 80, 47, 47, 27};
+		int[] num = {6, 5, 5};
 		System.out.println("MajorityElement : " + majorityElement(num));
 		System.out.println("majorityElementSolution : "
 				+ majorityElementSolution(num));
@@ -30,15 +30,16 @@ public class Solution {
 
 	public static int majorityElement(int[] nums) {
 		int count = 0, major = 0;
-		for (int i = 0; i < nums.length; i++) {
-			int num = nums[i];
+		for (int num : nums) {
 			if (count == 0) {
 				major = num;
 				count = 1;
-			} else if (major != num) {
-				count--;
 			} else {
-				count++;
+				if (major != num) {
+					count--;
+				} else {
+					count++;
+				}
 			}
 		}
 		return major;
