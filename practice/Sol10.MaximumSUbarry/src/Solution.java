1b55ce0d2b2c52cc0a00d5c9cb7662739c5131c6
hyogij <hyogij@gmail.com>
2016-02-16 14:58:00 +0900

    Find the contiguous subarray within an array which has the largest sum

---
+++ practice/Sol10.MaximumSUbarry/src/Solution.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+/*
+ ==========================================================
+ Author : Hyogi Jung(hyogij@gmail.com)
+ Date : 2016.02.16
+ Link : https://leetcode.com/problems/maximum-subarray/
+ ==========================================================
+ */
+public class Solution {
+	/*
+	 * Find the contiguous subarray within an array (containing at least one
+	 * number) which has the largest sum.
+	 * 
+	 * For example, given the array [-2,1,-3,4,-1,2,1,-5,4], the contiguous
+	 * subarray [4,-1,2,1] has the largest sum = 6.
+	 * 
+	 * click to show more practice.
+	 * 
+	 * More practice: If you have figured out the O(n) solution, try coding
+	 * another solution using the divide and conquer approach, which is more
+	 * subtle.
+	 * 
+	 * Subscribe to see which companies asked this question
+	 */
+	public static void main(String[] args) {
+		int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+		System.out.println("getMaximumSubarray : " + getMaximumSubarray(arr));
+	}
+
+	public static int getMaximumSubarray(int[] arr) {
+		int max = Integer.MIN_VALUE;
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+			if (sum < 0) {
+				sum = 0;
+			} else if (sum > max) {
+				max = sum;
+			}
+		}
+		return max;
+	}
+	
+	public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+		int sum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			if (sum > maxSum) {
+				maxSum = sum;
+			}
+			if (sum < 0) {
+				sum = 0;
+			}
+		}
+		return maxSum;
+    }
+}
