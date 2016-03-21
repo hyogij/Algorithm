7b09cd9732ac53e21f7cfc4ed121b4f59e849031
hyogij <hyogij@gmail.com>
2016-03-21 10:03:35 +0900

    Improve an algorithm using stack

--- leetcode.com/Day19.LevelOrderTraversal2/src/leetCode/LevelOrderTraversal2.java
+++ leetcode.com/Day19.LevelOrderTraversal2/src/leetCode/LevelOrderTraversal2.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 /*
 ==========================================================
@@ -47,6 +48,44 @@ public class LevelOrderTraversal2 {
 
 		Collections.reverse(list);
 		System.out.println("levelOrder " + list.toString());
+		
+		list = levelOrderAdvanced(root);
+		System.out.println("levelOrder " + list.toString());
+	}
+	
+	// Level order traversal using queue
+	public static List<List<Integer>> levelOrderAdvanced(TreeNode root) {
+		List<List<Integer>> list = new ArrayList<List<Integer>>();
+		if (root == null) {
+			return list;
+		}
+
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		Stack<List<Integer>> stack = new Stack<List<Integer>>();
+		queue.add(root);
+		while (queue.isEmpty() == false) {
+			List<Integer> current = new ArrayList<Integer>();
+
+			// Get the number of nodes in current depth
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+				current.add(node.val);
+				if (node.left != null)
+					queue.add(node.left);
+				if (node.right != null)
+					queue.add(node.right);
+			}
+			stack.add(current);
+		}
+
+		int size = stack.size();
+		for (int i = 0; i < size; i++) {
+			List<Integer> current = stack.pop();
+			list.add(current);
+		}
+
+		return list;
 	}
 
 	public static List<List<Integer>> levelOrder(TreeNode root) {
