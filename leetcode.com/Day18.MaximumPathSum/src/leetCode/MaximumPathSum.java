9f1eabad51f74943bab0da72fc42f284306eac75
hyogij <hyogij@gmail.com>
2016-03-25 17:00:27 +0900

    Add a more simple solution

--- leetcode.com/Day18.MaximumPathSum/src/leetCode/MaximumPathSum.java
+++ leetcode.com/Day18.MaximumPathSum/src/leetCode/MaximumPathSum.java
@@ -7,6 +7,7 @@ package leetCode;
  Date : 2015.08.18
  Question Number : 71
  Link : https://leetcode.com/problems/binary-tree-maximum-path-sum/
+ http://www.programcreek.com/2013/02/leetcode-binary-tree-maximum-path-sum-java/
  ==========================================================
  */
 
@@ -27,9 +28,12 @@ public class MaximumPathSum {
 		tree.right = new TreeNode(7);
 		tree.left.left = new TreeNode(10);
 		tree.left.right = new TreeNode(4);
+		tree.left.right.right = new TreeNode(2);
+		tree.left.right.right.right = new TreeNode(12);
 
 		printGraph(tree);
 		System.out.println("maxPathSum : " + maxPathSum(tree));
+		System.out.println("maxPathSumSimple : " + maxPathSum(tree));
 	}
 
 	// 1) Recursively solve this problem
@@ -48,6 +52,25 @@ public class MaximumPathSum {
 		int right = Math.max(0, maxPathDown(node.left));
 		maxValue = Math.max(maxValue, left + right + node.val);
 		return Math.max(left, right) + node.val;
+	}
+	
+	static int tmpValue = Integer.MIN_VALUE;
+	public static int maxPathSumSimple(TreeNode root) {
+		maxPathSumSimpleHelper(root);
+		return tmpValue;
+	}
+	
+	public static int maxPathSumSimpleHelper(TreeNode node) {
+		if (node == null)
+			return 0;
+		int left = maxPathSumSimpleHelper(node.left);
+		int right = maxPathSumSimpleHelper(node.right);
+
+		int current = Math.max(node.val,
+				Math.max(left + node.val, right + node.val));
+		tmpValue = Math.max(tmpValue,
+				Math.max(current, left + right + node.val));
+		return current;
 	}
 
 	public static void printGraph(TreeNode root) {
