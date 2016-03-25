5995de28cdea9dd30b06f3cf46e879e9642a2e9b
hyogij <hyogij@gmail.com>
2016-03-25 10:23:55 +0900

    Search for a value in a m*n matrix

--- leetcode.com/Day35.SearchMatrix/src/leetCode/SearchMatrix.java
+++ leetcode.com/Day35.SearchMatrix/src/leetCode/SearchMatrix.java
@@ -25,20 +25,81 @@ public class SearchMatrix {
 	 * Given target = 3, return true.
 	 */
 	public static void main(String[] args) {
-		int[][] matrix = {{1, 3, 5, 7}};
-		// , {10, 11, 16, 20}, {23, 30, 34, 50}};
+		int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}};
 
+		System.out.println("searchMatrixSolution : "
+				+ searchMatrixSolution(matrix, 1));
 		System.out.println("searchMatrix : " + searchMatrix(matrix, 1));
+		System.out.println("searchMatrixSolution : "
+				+ searchMatrixSolution(matrix, 2));
 		System.out.println("searchMatrix : " + searchMatrix(matrix, 2));
+		System.out.println("searchMatrixSolution : "
+				+ searchMatrixSolution(matrix, 3));
 		System.out.println("searchMatrix : " + searchMatrix(matrix, 3));
+		System.out.println("searchMatrixSolution : "
+				+ searchMatrixSolution(matrix, 7));
 		System.out.println("searchMatrix : " + searchMatrix(matrix, 7));
-		System.out.println("searchMatrix : " + searchMatrix(matrix, 8));
+		System.out.println("searchMatrixSolution : "
+				+ searchMatrixSolution(matrix, 12));
+		System.out.println("searchMatrix : " + searchMatrix(matrix, 12));
+		System.out.println("searchMatrixSolution : "
+				+ searchMatrixSolution(matrix, 16));
+		System.out.println("searchMatrix : " + searchMatrix(matrix, 16));
+		System.out.println("searchMatrixSolution : "
+				+ searchMatrixSolution(matrix, 22));
 		System.out.println("searchMatrix : " + searchMatrix(matrix, 22));
+		System.out.println("searchMatrixSolution : "
+				+ searchMatrixSolution(matrix, 24));
 		System.out.println("searchMatrix : " + searchMatrix(matrix, 24));
+		System.out.println("searchMatrixSolution : "
+				+ searchMatrixSolution(matrix, 34));
 		System.out.println("searchMatrix : " + searchMatrix(matrix, 34));
 	}
 
 	public static boolean searchMatrix(int[][] matrix, int target) {
+		if (matrix.length == 0 || matrix[0].length == 0) {
+			return false;
+		}
+
+		int row = findRowMatrix(matrix, target, 0, matrix.length - 1);
+		return findColMatrix(matrix, target, row, 0, matrix[row].length - 1);
+	}
+
+	public static int findRowMatrix(int[][] matrix, int target, int bottom,
+			int top) {
+		if (top - bottom <= 1) {
+			if (matrix[top][0] <= target)
+				return top;
+			else
+				return bottom;
+		}
+
+		int mid = (top - bottom) / 2 + bottom;
+		if (matrix[mid][0] == target) {
+			return mid;
+		} else if (matrix[mid][0] < target) {
+			return findRowMatrix(matrix, target, mid, top);
+		} else {
+			return findRowMatrix(matrix, target, bottom, mid - 1);
+		}
+	}
+
+	public static boolean findColMatrix(int[][] matrix, int target, int row,
+			int left, int right) {
+		if (right < left)
+			return false;
+
+		int mid = (right - left) / 2 + left;
+		if (matrix[row][mid] == target) {
+			return true;
+		} else if (matrix[row][mid] < target) {
+			return findColMatrix(matrix, target, row, mid + 1, right);
+		} else {
+			return findColMatrix(matrix, target, row, left, mid - 1);
+		}
+	}
+
+	public static boolean searchMatrixSolution(int[][] matrix, int target) {
 		if (matrix.length == 0) {
 			return false;
 		}
