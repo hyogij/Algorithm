4ec8cb596e4018d0af93019424f6838f615ce5c4
hyogij <hyogij@gmail.com>
2016-03-30 17:30:37 +0900

    Find all distinct solutions to the nqueens puzzle

---
+++ practice/Sol81.nQueens/src/Solution.java
@@ -0,0 +1,44 @@
+import java.util.List;
+
+/*
+ ==========================================================
+ Author : Hyogi Jung(hyogij@gmail.com)
+ Date : 2016.03.30
+ Problem : https://leetcode.com/problems/n-queens/
+ ==========================================================
+ */
+
+public class Solution {
+	/*
+	 * The n-queens puzzle is the problem of placing n queens on an n×n
+	 * chessboard such that no two queens attack each other.
+	 * 
+	 * Given an integer n, return all distinct solutions to the n-queens puzzle.
+	 * 
+	 * Each solution contains a distinct board configuration of the n-queens'
+	 * placement, where 'Q' and '.' both indicate a queen and an empty space
+	 * respectively.
+	 * 
+	 * For example, There exist two distinct solutions to the 4-queens puzzle:
+	 * 
+	 * [[".Q..", // Solution 1 
+	 * "...Q", 
+	 * "Q...", 
+	 * "..Q."],
+	 * 
+	 * ["..Q.", // Solution 2 
+	 * "Q...", 
+	 * "...Q", 
+	 * ".Q.."] ]
+	 */
+	public static void main(String[] args) {
+
+		// printGraph(tree);
+		System.out.println("isPairPresent " + isPairPresent(tree, 33));
+	}
+
+	public List<List<String>> solveNQueens(int n) {
+		// 배열을 만들어서, 가능한 조건을 모두 넣는다 
+	}
+
+}
