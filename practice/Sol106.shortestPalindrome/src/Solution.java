12533ea2beba598225fa6bb7184e36e7cffb9a94
hyogij <hyogij@gmail.com>
2016-04-06 16:15:23 +0900

    Add a naive solution to find shortest palindrome

--- practice/Sol106.shortestPalindrome/src/Solution.java
+++ practice/Sol106.shortestPalindrome/src/Solution.java
@@ -15,25 +15,54 @@ public class Solution {
 	 * 
 	 * For example:
 	 * 
-	 * Given "aacecaaa", return "aaacecaaa". 
-	 * Given "abcd", return "dcbabcd".
+	 * Given "aacecaaa", return "aaacecaaa". Given "abcd", return "dcbabcd".
 	 */
 	public static void main(String[] args) {
 		System.out.println(shortestPalindrome("aacecaaa"));
+		System.out.println(shortestPalindromeNaive("aacecaaa"));
 		System.out.println(shortestPalindrome("abcd"));
+		System.out.println(shortestPalindromeNaive("abcd"));
 		System.out.println(shortestPalindrome("aaaaa"));
+		System.out.println(shortestPalindromeNaive("aaaaa"));
 		System.out.println(shortestPalindrome("aaaa"));
+		System.out.println(shortestPalindromeNaive("aaaa"));
+	}
+
+	public static String shortestPalindromeNaive(String s) {
+		StringBuffer sb = new StringBuffer(s);
+		
+		if (isPalindrome(s))
+			return s;
+
+		for (int i = 0; i < s.length(); i++) {
+			String palindrom = sb.reverse().substring(0, i + 1) + s;
+			if (isPalindrome(palindrom))
+				return palindrom;
+		}
+
+		while (true) {
+			if (isPalindrome(s)) {
+				return s;
+			}
+			s = s.charAt(s.length() - 1) + s;
+		}
+	}
+
+	public static boolean isPalindrome(String s) {
+		for (int i = 0; i < s.length() / 2; i++) {
+			if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public static String shortestPalindrome(String s) {
-		if(s == null || s.length() == 0)
+		if (s == null || s.length() == 0)
 			return "";
-		
-		String temp = s + "#" +new StringBuilder(s).reverse().toString();
-		int[] table = makeNext(temp);
 
-//		System.out.println(temp);
-//		System.out.println(Arrays.toString(table));
+		String temp = s + "#" + new StringBuilder(s).reverse().toString();
+		int[] table = makeNext(temp);
 
 		// Get the maximum palin part in s starts from 0
 		return new StringBuilder(s.substring(table[table.length - 1]))
@@ -42,7 +71,7 @@ public class Solution {
 
 	public static int[] makeNext(String s) {
 		int[] next = new int[s.length()];
-		
+
 		int index = 0;
 		for (int i = 1; i < s.length();) {
 			if (s.charAt(index) == s.charAt(i)) {
