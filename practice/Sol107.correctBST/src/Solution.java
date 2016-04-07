a6f982d7f717cc61e6aee830aa1906330f8d6a2a
hyogij <hyogij@gmail.com>
2016-04-07 16:09:57 +0900

    Fix the BST. There are two of swapped nodes in BST

--- practice/Sol107.correctBST/src/Solution.java
+++ practice/Sol107.correctBST/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /*
  ==========================================================
  Author : Hyogi Jung(hyogij@gmail.com)
@@ -30,22 +32,95 @@ public class Solution {
 		tree.left.right = new TreeNode(3);
 		tree.right.left = new TreeNode(7);
 		tree.right.right = new TreeNode(12);
-		
+
 		inorder(tree);
 		System.out.println();
-		
+
 		correctBST(tree);
-		
+
 		inorder(tree);
 		System.out.println();
 	}
-	
+
 	// A function to fix a given BST where two nodes are swapped. This
 	// function uses correctBSTUtil() to find out two nodes and swaps the
 	// nodes to fix the BST
 	public static void correctBST(TreeNode root) {
+		if (root == null)
+			return;
+
+		TreeNode swap1 = null, swap2 = null;
+		swap1 = findSwapNode(root);
+		swap2 = findSwapNodeReverse(root);
+
+		swap(swap1, swap2);
 	}
-	
+
+	private static TreeNode findSwapNode(TreeNode root) {
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+
+		// Traverse binary tree from left to right
+		TreeNode current = root, prev = null, swap = null;
+		while (current != null) {
+			stack.push(current);
+			current = current.left;
+		}
+
+		while (!stack.isEmpty()) {
+			current = stack.pop();
+
+			// Find swap node in inorder traverse
+			if (prev != null && prev.val > current.val) {
+				swap = prev;
+				break;
+			}
+
+			prev = current;
+			current = current.right;
+			while (current != null) {
+				stack.push(current);
+				current = current.left;
+			}
+		}
+		return swap;
+	}
+
+	private static TreeNode findSwapNodeReverse(TreeNode root) {
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+
+		// Traverse binary tree from left to right
+		TreeNode current = root, prev = null, swap = null;
+		while (current != null) {
+			stack.push(current);
+			current = current.right;
+		}
+
+		while (!stack.isEmpty()) {
+			current = stack.pop();
+
+			// Find swap node in reverse inorder traverse
+			if (prev != null && prev.val < current.val) {
+				swap = prev;
+				break;
+			}
+
+			prev = current;
+			current = current.left;
+			while (current != null) {
+				stack.push(current);
+				current = current.right;
+			}
+		}
+		return swap;
+	}
+
+	private static void swap(TreeNode swap1, TreeNode swap2) {
+		int tmp = swap1.val;
+		swap1.val = swap2.val;
+		swap2.val = tmp;
+
+	}
+
 	public static void inorder(TreeNode root) {
 		if (root != null) {
 			inorder(root.left);
@@ -65,5 +140,4 @@ public class Solution {
 			val = x;
 		}
 	}
-
 }
