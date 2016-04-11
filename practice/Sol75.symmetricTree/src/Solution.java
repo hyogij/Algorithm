1a8a6e461976d0a2aabd23ee9e2944be5098f40e
hyogij <hyogij@gmail.com>
2016-04-11 11:14:07 +0900

    Add a recursive solution

--- practice/Sol75.symmetricTree/src/Solution.java
+++ practice/Sol75.symmetricTree/src/Solution.java
@@ -38,6 +38,27 @@ But the following is not:
 
 		System.out.println("isSymmetric " + isSymmetric(tree));
 	}
+	
+	public static boolean isSymmetricRecursive(TreeNode root) {
+		if (root == null)
+			return true;
+
+		return isSymmetricRecursiveHepler(root.left, root.right);
+	}
+
+	public static boolean isSymmetricRecursiveHepler(TreeNode root1,
+			TreeNode root2) {
+		if (root1 == null && root2 == null)
+			return true;
+		if (root1 == null || root2 == null)
+			return false;
+
+		if (root1.val == root2.val)
+			return isSymmetricRecursiveHepler(root1.left, root2.right)
+					&& isSymmetricRecursiveHepler(root1.right, root2.left);
+		
+		return false;
+	}
 
 	public static boolean isSymmetric(TreeNode root) {
 		if (root == null)
