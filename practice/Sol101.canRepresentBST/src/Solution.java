5d9672f70c8af613e9f2bb1b6e3a36f77d70bef9
hyogij <hyogij@gmail.com>
2016-04-13 14:57:38 +0900

    Add another solution using stack which time complexity is O(n)

--- practice/Sol101.canRepresentBST/src/Solution.java
+++ practice/Sol101.canRepresentBST/src/Solution.java
@@ -80,4 +80,33 @@ public class Solution {
 		}
 		return false;
 	}
+	
+	public static boolean canBuildTreeStack(int[] pre) {
+		// Create an empty stack
+		Stack<Integer> s = new Stack<Integer>();
+
+		// Initialize current root as minimum possible value
+		int root = Integer.MIN_VALUE;
+
+		// Traverse given array
+		for (int i = 0; i < pre.length; i++) {
+			// If we find a node who is on right side and smaller than root,
+			// return false
+			if (pre[i] < root) {
+				return false;
+			}
+
+			// If pre[i] is in right subtree of stack top, Keep removing items
+			// smaller than pre[i] and make the last removed item as new root.
+			while (!s.empty() && s.peek() < pre[i]) {
+				root = s.peek();
+				s.pop();
+			}
+
+			// At this point either stack is empty or pre[i] is smaller than
+			// root, push pre[i]
+			s.push(pre[i]);
+		}
+		return true;
+	}
 }
